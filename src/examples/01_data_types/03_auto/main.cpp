508cd87b3ef1915bd70132a693746a49daee4df0
sdw94 <143123602+sdw94@users.noreply.github.com>
2023-09-01 01:27:52 +0000

    hw1: multiply numbers func and test cases

--- src/examples/01_data_types/03_auto/main.cpp
+++ src/examples/01_data_types/03_auto/main.cpp
@@ -1,5 +1,12 @@
+#include<iostream>
+
+using std::cout;
+
 int main()
 {
+    auto num = 5;
+    
+    cout<<"auto value is: "<<num<<"\n";  
 
     return 0;
 }
--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,12 +1,26 @@
 ﻿//write include statements
-
+#include<iostream>
+#include "data_types.h"
 
 //write namespace using statement for cout
-
+using std::cin; using std::cout;
 
 int main()
 {
-	
+	auto num = 0;
+
+	cout<<"Enter a numer: ";
+	cin>>num;
+
+	auto result = 0;
+	result = multiply_numbers(num);
+
+	cout<<"Result is: "<<result<<"\n";
+
+	auto num1 = 4;
+	result = multiply_numbers(num1);
+
+	cout<<"Result is: "<<"/n";
 
 	return 0;
 }
