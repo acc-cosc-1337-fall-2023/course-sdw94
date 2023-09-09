66f406e3c660db00f03c35b630a13fd6cde23d8b
sdw94 <143123602+sdw94@users.noreply.github.com>
2023-09-09 15:37:24 +0000

    HW2

--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -3,10 +3,41 @@
 #include "hwexpressions.h"
 
 //write namespace using statement for cout
-using std::cout; using std::cin;
+using std::cin; using std::cout;
+
+double meal_amount;
+double sales_tax;
+double tip_rate;
+double tax_amount;
+double total;
 
 int main()
 {
+
+	auto num = 0;
+
+	cout<<"Enter a number: ";
+	cin>>num;
+
+	auto result = 0;
+	result = double meal_amount(num);
+
+	result = get_sales_tax_amount(meal_amount);
+	double tax_amount = result;
+
+	double tip_rate;
+	cout<<"Enter the tip rate: ";
+	cin>>tip_rate;
+
+	result = double get_tip_amount(double meal_amount, double tip_rate);
+	double tip_amount = result;
+
+	auto total = tip_amount + tax_amount + meal_amount;
+
+	cout<<"The meal amount is: "<<meal_amount<<"\n";
+	cout<<"The sales tax is: "<<sales_tax<<"\n";
+	cout<<"The tip amount is: "<<tip_amount<<"\n";
+	cout<<"The total is: "<<total;
 	
 
 	return 0;
