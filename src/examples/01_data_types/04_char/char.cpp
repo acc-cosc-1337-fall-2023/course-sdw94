ff6b677e00aaf04f583a477ae28b50fa402d910b
sdw94 <143123602+sdw94@users.noreply.github.com>
2023-09-09 00:27:04 +0000

    HW2 write tests

--- src/examples/01_data_types/04_char/char.cpp
+++ src/examples/01_data_types/04_char/char.cpp
@@ -1,5 +1,5 @@
 //cpp
-
+#include "char.h"
 
 /*
 Function get_char_ascii_value with a char parameter
@@ -7,3 +7,14 @@ Function get_char_ascii_value with a char parameter
 @param char-a character
 @return the ASCII value of the character
 */
+int get_char_ascii_value(char ch)
+{
+    return ch;
+}
+
+bool get_and_result(int num1, int num2)
+{
+    bool result = num1 == 3 && num2 ==5;
+
+    return result;
+}
--- src/homework/02_expressions/hwexpressions.cpp
+++ src/homework/02_expressions/hwexpressions.cpp
@@ -8,6 +8,15 @@ int add_numbers(int num1, int num2)
 
 //write function code here
 
+double multiply_tax_rate(double tax_rate, double meal_amount)
+{
+	result = tax_rate * meal_amount;
+	return result;//1-b hw2
+}
 
-
+double multiply_rates(double meal_amount, double tip_rate)
+{
+	result = meal_amount * tip_rate
+	return result;//1-d hw2
+}
 
