8d6390b5050752a7b94631f7ff3b4a6a8eed4b61
sdw94 <143123602+sdw94@users.noreply.github.com>
2023-09-23 22:53:11 +0000

    HW4

--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,8 +1,22 @@
 //write include statements
+#include"repetition.h"
+#include<iostream>
+#include<string>
 
 //write using statements
 
+using std::cout; using std::cin;
+
 int main() 
 {
+	int x = 0;
+	do
+	{
+		cout <<  "1 - Factorial\n" << "2 - Greatest Common Divisor\n" << "3 - Exit";
+	} while (x < 3);
+
+	if (x=3)
+		cout << "Are you sure you want to exit?";
+	
 	return 0;
 }
--- src/homework/04_repetition/repetition.cpp
+++ src/homework/04_repetition/repetition.cpp
@@ -1,3 +1,35 @@
 //add include statements
+#include"repetition.h"
+#include<string>
+
+
+using std::cout;
 
 //add function(s) code here
+
+
+int factorial(int num)
+{
+    auto sum = 0;
+
+    while(num > 0)
+    {
+        sum = sum + num * num;
+        num = num - 1;
+    }
+
+    return sum;
+}
+
+int gcd(int num1, int num2)
+{
+    while(num1 =! num2)
+    {
+        if (num1 < num2)
+            num1 == num2 && num2 == num1;
+        if (num1 > num2)
+            num1 - num2;
+    }
+
+    return num1;
+}
